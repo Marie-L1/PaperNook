7ce8d00b4b6f074b6d4d02facb004b2e6aa389fa
Maria Lukowich <mlukowich27@gmail.com>
2025-01-09 16:03:10 -0700

    initializing the develop branch

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styles from "./page.module.scss";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 function page() {
+  const router = useRouter();
+
   return (
     <div>
-      
+
     </div>
   )
 }
