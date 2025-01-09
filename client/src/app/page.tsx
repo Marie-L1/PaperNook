c592d345adb5f283613018fbf20869ec0623dfd3
Maria Lukowich <mlukowich27@gmail.com>
2025-01-09 16:14:46 -0700

    setting up the basic homepage layout and global styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,7 +7,25 @@ function page() {
   const router = useRouter();
 
   return (
-    <div>
+    <div className="homepage">
+      <div className="homepage__logo">
+        <Image src="/images/PaperNook-logo.svg"
+          alt="logo"
+          width={500}
+          height={500}
+        />
+        <p className="homepage__logo-text">PaperNook</p>
+
+        <div className="homepage__buttons">
+          <button className="homepage__buttons__button" onClick={() => router.push("/login")}>Login</button>
+          <button className="homepage__buttons__button" onClick={() => router.push("/register")}>Register</button>
+        </div>
+      </div>
+
+      <Image src="/images/background-homepage.jpg" 
+        alt="background-homepage"
+        width={1000}
+        height={500}/>
 
     </div>
   )
